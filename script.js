9ea66343e13044a95e930c12094cc8de500da4fe
DrGugl <98596948+DrGugl@users.noreply.github.com>
2024-05-27 20:00:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-document.getElementById('toggle-dark-mode').addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-    this.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+    const darkMode = localStorage.getItem('dark-mode');
+
+    if (darkMode === 'enabled') {
+        document.body.classList.add('dark-mode');
+        toggleButton.textContent = '☀️';
+    }
+
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('dark-mode', 'enabled');
+            toggleButton.textContent = '☀️';
+        } else {
+            localStorage.setItem('dark-mode', 'disabled');
+            toggleButton.textContent = '🌙';
+        }
+    });
 });
