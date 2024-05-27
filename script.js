82c437d62c53c339f7fd2fa19e17ba5f36bbece5
DrGugl <98596948+DrGugl@users.noreply.github.com>
2024-05-27 19:55:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById('toggle-dark-mode').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
+    this.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
 });
